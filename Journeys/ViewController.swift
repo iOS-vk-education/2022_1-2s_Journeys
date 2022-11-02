0cbc5bed6c89090636759c99bc9fc2bcb776107a
Ischenko <nastyaisc849@gmail.com>
2022-11-02 19:10:50 +0300

    Progect and generamba created

---
+++ Journeys/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Journeys
+//
+//  Created by Сергей Адольевич on 02.11.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
