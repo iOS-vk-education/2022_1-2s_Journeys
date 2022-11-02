20c4758d39b1e91a778ba052779efe1d8f5d44b7
Ischenko <nastyaisc849@gmail.com>
2022-11-02 22:52:36 +0300

    Fixed platform

--- Journeys/ViewController.swift
+++ Journeys/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by Сергей Адольевич on 02.11.2022.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
