094853086aba9f03e360d16d99fb68ad27fa95a9
Ischenko <nastyaisc849@gmail.com>
2022-11-03 17:42:28 +0300

    Trips module created

---
+++ Journeys/Modules/Trips/Presenter/TripsModuleIO.swift
@@ -0,0 +1,16 @@
+//
+//  TripsModuleIO.swift
+//  Journeys
+//
+//  Created by Nastya Ischenko on 03/11/2022.
+//
+
+// MARK: - Trips ModuleInput
+
+protocol TripsModuleInput: AnyObject {
+}
+
+// MARK: - Trips ModuleOutput
+
+protocol TripsModuleOutput: AnyObject {
+}
